07e0229f8f30fe40084d08f08853473cab5df1cb
Muhammad Fatihul Qolbi Ash Shiddiqi <fatihulqolbi01@gmail.com>
2025-03-04 22:40:09 +0700

    first commit

---
+++ Latihan/Pertemuan 1/latian1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mhs_private{
+  //Access specifier
+  private:
+  string mhs_name;
+
+  public:
+
+  // setter for mhs_name
+  void set_name (string name){
+    mhsna
+  }
+  //Data members
+  string MHSname;
+
+  //Member functions()
+  void printname() {
+    cout << "MHS name is:" << MHSname;
+  }
+}; 
+
+int main() {
+  // Declare an object of class geeks
+  MHS mhs1;
+  MHS mhs2;
+
+  // Accessing data member 
+  mhs1.MHSname = "Rahmat Budi";
+  mhs2 MHSname = "Muhammad Ali";
+  // Accessing member function
+  
+
+  return 0;
+}
